87148869d1fec2b3f737df9df2d3b82066a45913
QuietWolf14 <jellydonut14@gmail.com>
2022-04-10 19:31:46 -0600

    make a few small tweaks

--- js/people_counter.js
+++ js/people_counter.js
@@ -1,17 +1,17 @@
 function increment() {
-    let count = parseInt(document.getElementById("count-el").innerText)
+    let count = parseInt(document.getElementById("count-el").textContent)
 
     count = count + 1
 
-    document.getElementById("count-el").innerText = count
+    document.getElementById("count-el").textContent = count
 }
 
 function save() {
-    let curr_count = parseInt(document.getElementById("count-el").innerText)
+    let curr_count = parseInt(document.getElementById("count-el").textContent)
 
     alert("The current count is " + curr_count)
 }
 
 function reset() {
-    document.getElementById("count-el").innerText = 0
+    document.getElementById("count-el").textContent = 0
 }
